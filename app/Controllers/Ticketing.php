8b1aedc66fb6ea7c8ab096e7c090a42dc8a34b80
Bayu S. Ajie P. Putra <bayu.setya.ajie@gmail.com>
2024-05-12 01:12:55 +0700

    add model laboratorium & fix function index

--- app/Controllers/Ticketing.php
+++ app/Controllers/Ticketing.php
@@ -2,11 +2,22 @@
 
 namespace App\Controllers;
 
+use App\Models\M_Laboratorium;
+
 class Ticketing extends BaseController
 {
 
+  protected $laboratorium;
+
+  public function __construct()
+  {
+    $this->laboratorium = new M_Laboratorium();
+  }
+
   public function index()
   {
-    echo 'hai';
+    $data['title'] = 'Trouble Ticket Laboratorium | SIM Laboratorium';
+    $data['laboratorium'] = $this->laboratorium->getAllLaboratorium();
+    return view('ticketing/v_index', $data);
   }
 }
