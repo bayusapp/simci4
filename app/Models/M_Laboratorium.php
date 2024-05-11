8865bb83066f3761da37ed2f34e65c7fd4b16b89
Bayu S. Ajie P. Putra <bayu.setya.ajie@gmail.com>
2024-05-12 00:21:08 +0700

    create model for laboratorium

---
+++ app/Models/M_Laboratorium.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class M_Laboratorium extends Model
+{
+
+  protected $table = 'laboratorium';
+
+  public function getAllLaboratorium()
+  {
+    return $this->findAll();
+  }
+}
