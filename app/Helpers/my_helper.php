60e6288dcb7b77521749f2fc4c8dfdd18ca1e573
Bayu S. Ajie P. Putra <bayu.setya.ajie@gmail.com>
2024-05-12 23:42:07 +0700

    add helper for check ip & check isp

--- app/Helpers/my_helper.php
+++ app/Helpers/my_helper.php
@@ -17,3 +17,56 @@ if (!function_exists('getAddress')) {
     }
   }
 }
+
+if (!function_exists('check_ip')) {
+  function check_ip()
+  {
+    if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+      $ip_address = $_SERVER['HTTP_CLIENT_IP'];
+    } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+      $ip_address = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    } else {
+      $ip_address = $_SERVER['REMOTE_ADDR'];
+    }
+    // IP TUNE
+    // $json = file_get_contents("http://ipinfo.io/103.233.100.236/geo");
+    // IP ITB
+    // $json = file_get_contents("http://ipinfo.io/167.205.22.106/geo");
+    // IP Non TUNE
+    // $json = file_get_contents("http://ipinfo.io/114.122.100.20/geo");
+    $json = file_get_contents("http://ipinfo.io/{$ip_address}/geo");
+    $details = json_decode($json, true);
+    return $details;
+  }
+}
+
+if (!function_exists('check_isp')) {
+  function check_isp()
+  {
+    if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+      $ip_address = $_SERVER['HTTP_CLIENT_IP'];
+    } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+      $ip_address = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    } else {
+      $ip_address = $_SERVER['REMOTE_ADDR'];
+    }
+    // IP TUNE
+    // $json = file_get_contents("http://ipinfo.io/103.233.100.236/geo");
+    // IP ITB
+    $json = file_get_contents("http://ipinfo.io/167.205.22.106/geo");
+    // IP Non TUNE
+    // $json = file_get_contents("http://ipinfo.io/114.122.100.20/geo");
+    // $json = file_get_contents("http://ipinfo.io/{$ip_address}/geo");
+    $details = json_decode($json, true);
+    $tmp = explode(' ', $details['org']);
+    $org  = '';
+    for ($i = 1; $i < count($tmp); $i++) {
+      if ($i < (count($tmp) - 1)) {
+        $org .= $tmp[$i] . ' ';
+      } else {
+        $org .= $tmp[$i];
+      }
+    }
+    return $org;
+  }
+}
