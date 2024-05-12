1506a187f2df5ed8f0487a1a5f2fc7aba7cb6540
Bayu S. Ajie P. Putra <bayu.setya.ajie@gmail.com>
2024-05-12 12:26:29 +0700

    add function_exists to getAddress function

--- app/Helpers/my_helper.php
+++ app/Helpers/my_helper.php
@@ -1,17 +1,19 @@
 <?php
 
-function getAddress($latlng)
-{
-  if ($latlng != null) {
-    //google map api url
-    $url = "https://maps.google.com/maps/api/geocode/json?latlng=$latlng&key=AIzaSyA-BQS8hr9-K1fOsgTpvWAFrh-ocZk3DcQ";
+if (!function_exists('getAddress')) {
+  function getAddress($latlng)
+  {
+    if ($latlng != null) {
+      //google map api url
+      $url = "https://maps.google.com/maps/api/geocode/json?latlng=$latlng&key=AIzaSyA-BQS8hr9-K1fOsgTpvWAFrh-ocZk3DcQ";
 
-    // send http request
-    $geocode = file_get_contents($url);
-    $json = json_decode($geocode);
-    $address = $json->results[0];
-    return $address;
-  } else {
-    return 'off';
+      // send http request
+      $geocode = file_get_contents($url);
+      $json = json_decode($geocode);
+      $address = $json->results[0];
+      return $address;
+    } else {
+      return 'off';
+    }
   }
 }
