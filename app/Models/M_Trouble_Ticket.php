dd7a39669eb3a95d72e82df5192e3b3283c771af
Bayu S. Ajie P. Putra <bayu.setya.ajie@gmail.com>
2024-05-13 13:30:15 +0700

    add function trouble ticket detail

--- app/Models/M_Trouble_Ticket.php
+++ app/Models/M_Trouble_Ticket.php
@@ -4,8 +4,15 @@ namespace App\Models;
 
 use CodeIgniter\Model;
 
-class M_Trouble_Ticker extends Model
+class M_Trouble_Ticket extends Model
 {
   protected $table = 'trouble_ticket';
   protected $allowedFields = ['tanggal_kendala', 'kategori_informan', 'nama_informan', 'no_informan', 'keluhan', 'status', 'ip_address', 'browser', 'platform', 'alamat_geolocation', 'isp', 'lat_long', 'id_lab'];
+
+  public function getTroubleTicketDetail($id)
+  {
+    $this->join('laboratorium', 'trouble_ticket.id_lab = laboratorium.id_lab');
+    $this->where('substring(sha1(id_trouble), 8, 7)', $id);
+    return $this->first();
+  }
 }
