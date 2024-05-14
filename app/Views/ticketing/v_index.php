04bd83e4cb028f307898d16c38032accf5608f99
Bayu S. Ajie P. Putra <bayu.setya.ajie@gmail.com>
2024-05-14 10:22:37 +0700

    remove footer

--- app/Views/ticketing/v_index.php
+++ app/Views/ticketing/v_index.php
@@ -94,15 +94,6 @@
       </div>
     </div>
     <div id='map'></div>
-    <hr />
-    <div class="row">
-      <div class="col-md-6">
-        Copyright Example Company
-      </div>
-      <div class="col-md-6 text-right">
-        <small>© 2014-2015</small>
-      </div>
-    </div>
   </div>
   <div class="footer">
     <div>
