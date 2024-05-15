baf8e9f35ebedc3da9205801b379b8c79448905b
Bayu S. Ajie P. Putra <bayu.setya.ajie@gmail.com>
2024-05-15 10:39:53 +0700

    add function to check username already exist or not

--- app/Models/M_Users.php
+++ app/Models/M_Users.php
@@ -10,6 +10,12 @@ class M_Users extends Model
   protected $table = 'users';
   protected $allowedFields = ['username', 'password', 'jenis_akses', 'jabatan', 'status_akun', 'nip_laboran', 'id_aslab'];
 
+  public function getUsername($username)
+  {
+    $this->where('username', $username);
+    return $this->first();
+  }
+
   public function getUserByNIP($nip)
   {
     $this->where('nip_laboran', $nip);
