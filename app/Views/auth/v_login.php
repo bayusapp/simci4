87aca5931dc9ea4525b1c3fa66d08db15a4818f6
Bayu S. Ajie P. Putra <bayu.setya.ajie@gmail.com>
2024-05-17 15:23:12 +0700

    fix view login

--- app/Views/auth/v_login.php
+++ app/Views/auth/v_login.php
@@ -17,11 +17,8 @@
   <meta name="description" content="" />
   <meta name="keywords" content="">
   <meta name="author" content="Phoenixcoded" />
-  <!-- Favicon icon -->
-  <link rel="icon" href="assets/images/favicon.ico" type="image/x-icon">
-
-  <!-- vendor css -->
-  <link rel="stylesheet" href="assets/css/style.css">
+  <link rel="shortcut icon" href="<?= base_url() ?>assets/images/favicon.png" type="image/x-icon">
+  <link rel="stylesheet" href="<?= base_url() ?>assets/css/style.css">
 
 
 
@@ -33,35 +30,33 @@
   <div class="flex-grow-1">
     <div class="h-100 d-md-flex align-items-center auth-side-img">
       <div class="col-sm-10 auth-content w-auto">
-        <img src="assets/images/auth/auth-logo.png" alt="" class="img-fluid">
+        <!-- <img src="assets/images/auth/auth-logo.png" alt="" class="img-fluid">
         <h1 class="text-white my-4">Welcome Back!</h1>
-        <h4 class="text-white font-weight-normal">Signin to your account and get explore the Able pro Dashboard Template.<br />Do not forget to play with live customizer</h4>
+        <h4 class="text-white font-weight-normal">Signin to your account and get explore the Able pro Dashboard Template.<br />Do not forget to play with live customizer</h4> -->
       </div>
     </div>
     <div class="auth-side-form">
       <div class=" auth-content">
         <img src="assets/images/auth/auth-logo-dark.png" alt="" class="img-fluid mb-4 d-block d-xl-none d-lg-none">
-        <h3 class="mb-4 f-w-400">Signin</h3>
-        <div class="form-group mb-3">
-          <label class="floating-label" for="Email">Email address</label>
-          <input type="text" class="form-control" id="Email" placeholder="">
+        <img src="<?= base_url() ?>assets/images/logo.png" alt="" class="img-fluid mb-4 d-block d-xl-none d-lg-none">
+        <div style="text-align: center;">
+          <h4 class="mb-3 f-w-400">Login</h4>
         </div>
-        <div class="form-group mb-4">
-          <label class="floating-label" for="Password">Password</label>
-          <input type="password" class="form-control" id="Password" placeholder="">
-        </div>
-        <div class="custom-control custom-checkbox text-left mb-4 mt-2">
-          <input type="checkbox" class="custom-control-input" id="customCheck1">
-          <label class="custom-control-label" for="customCheck1">Save credentials.</label>
-        </div>
-        <button class="btn btn-block btn-primary mb-4">Signin</button>
+        <form action="<?= base_url() ?>auth/login" method="post">
+          <div class="form-group mb-3">
+            <label class="floating-label" for="username">Username</label>
+            <input type="text" class="form-control" id="username" name="username" value="<?= old('username') ?>">
+          </div>
+          <div class="form-group mb-4">
+            <label class="floating-label" for="password">Password</label>
+            <input type="password" class="form-control" id="password" name="password">
+          </div>
+          <button class="btn btn-block btn-primary mb-4">Login</button>
+        </form>
         <div class="text-center">
-          <div class="saprator my-4"><span>OR</span></div>
-          <button class="btn text-white bg-facebook mb-2 mr-2  wid-40 px-0 hei-40 rounded-circle"><i class="fab fa-facebook-f"></i></button>
-          <button class="btn text-white bg-googleplus mb-2 mr-2 wid-40 px-0 hei-40 rounded-circle"><i class="fab fa-google-plus-g"></i></button>
-          <button class="btn text-white bg-twitter mb-2  wid-40 px-0 hei-40 rounded-circle"><i class="fab fa-twitter"></i></button>
-          <p class="mb-2 mt-4 text-muted">Forgot password? <a href="auth-reset-password-img-side.html" class="f-w-400">Reset</a></p>
-          <p class="mb-0 text-muted">Don’t have an account? <a href="auth-signup-img-side.html" class="f-w-400">Signup</a></p>
+          <div class="saprator my-4"><span>ATAU</span></div>
+          <p class="mb-2 mt-4 text-muted">Lupa password? <a href="auth-reset-password-img-side.html" class="f-w-400">Reset</a></p>
+          <p class="mb-0 text-muted">Belum punya akun? <a href="auth-signup-img-side.html" class="f-w-400">Register</a></p>
         </div>
       </div>
     </div>
@@ -70,13 +65,32 @@
 <!-- [ signin-img ] end -->
 
 <!-- Required Js -->
-<script src="assets/js/vendor-all.min.js"></script>
-<script src="assets/js/plugins/bootstrap.min.js"></script>
-<script src="assets/js/ripple.js"></script>
-<script src="assets/js/pcoded.min.js"></script>
-
-
-
+<script src="<?= base_url() ?>assets/js/vendor-all.min.js"></script>
+<script src="<?= base_url() ?>assets/js/plugins/bootstrap.min.js"></script>
+<script src="<?= base_url() ?>assets/js/ripple.js"></script>
+<script src="<?= base_url() ?>assets/js/pcoded.min.js"></script>
+<script src="<?= base_url() ?>assets/js/plugins/sweetalert.min.js"></script>
+<?php if (!empty(session()->getFlashdata('error'))) : ?>
+  <script>
+    $(document).ready(function() {
+      swal("Gagal Login", "Harap lengkapi seluruh field", "error");
+    });
+  </script>
+<?php endif; ?>
+<?php if (!empty(session()->getFlashdata('not_found'))) : ?>
+  <script>
+    $(document).ready(function() {
+      swal("Gagal Login", "Username tidak ditemukan", "error");
+    });
+  </script>
+<?php endif; ?>
+<?php if (!empty(session()->getFlashdata('invalid_password'))) : ?>
+  <script>
+    $(document).ready(function() {
+      swal("Gagal Login", "Password tidak valid", "error");
+    });
+  </script>
+<?php endif; ?>
 </body>
 
 </html>
