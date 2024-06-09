0f54fc57ee6aa87d498a7aa72244557176173753
Bayu S. Ajie P. Putra <bayu.setya.ajie@gmail.com>
2024-06-09 14:24:52 +0700

    create beranda controller

---
+++ app/Controllers/Beranda.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\M_Laboran;
+
+class Beranda extends BaseController
+{
+
+  var $data;
+  protected $laboran;
+  protected $nama_laboran;
+
+  public function __construct()
+  {
+    if (session()->get('jenis_akses') != 'laboran') {
+      header("Location: " . base_url());
+    }
+    if (session()->get('nip_laboran')) {
+      $this->laboran = new M_Laboran();
+      $nip                = session()->get('nip_laboran');
+      $dataLaboran        = $this->laboran->getDataLaboran($nip);
+      $this->data = array(
+        'nip_laboran'   => $dataLaboran['nip_laboran'],
+        'nama_laboran'  => $dataLaboran['nama_laboran'],
+        'foto_laboran'  => $dataLaboran['foto_laboran']
+      );
+    } else {
+      header("Location: " . base_url());
+      die();
+    }
+  }
+
+  public function index()
+  {
+    $data           = $this->data;
+    $data['title']  = 'Beranda | SIM Laboratorium';
+    return view('beranda/v_index', $data);
+  }
+}
