70c2eca28b542083403c3ea224e4761bc5356682
Bayu S. Ajie P. Putra <bayu.setya.ajie@gmail.com>
2024-06-11 10:58:16 +0700

    add function ubahprofil

--- app/Controllers/Profil.php
+++ app/Controllers/Profil.php
@@ -34,9 +34,28 @@ class profil extends BaseController
   public function index()
   {
     $data   = $this->data;
-    $data['title']  = 'Pengaturan Profil | SIM Laboratorium';
-    $data['profil'] = $this->laboran->getDataLaboran(session()->get('nip_laboran'));
+    $data['title']        = 'Pengaturan Profil | SIM Laboratorium';
+    $data['profil']       = $this->laboran->getDataLaboran(session()->get('nip_laboran'));
     $data['list_laboran'] = $this->laboran->getListLaboranToProfile(session()->get('nip_laboran'));
     return view('profil/v_index', $data);
+  }
+
+  public function UbahProfil()
+  {
+    if (!$this->validate([
+      'username'        => ['rules' => 'required'],
+      'nama_laboran'    => ['rules' => 'required'],
+      'kontak_laboran'  => ['rules' => 'required'],
+      'email_laboran'   => ['rules' => 'required']
+    ])) {
+      session()->setFlashdata('error', 'Harap lengkapi seluruh field');
+      return redirect()->back()->withInput();
+    } else {
+      $username       = $this->request->getPost('username');
+      $nama_laboran   = $this->request->getPost('nama_laboran');
+      $kontak_laboran = $this->request->getPost('kontak_laboran');
+      $email_laboran  = $this->request->getPost('email_laboran');
+      // $cek_data       = $this->laboran->getData
+    }
   }
 }
