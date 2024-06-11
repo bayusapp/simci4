4bcaf6d2a9eb1d9d2912dc8e2a295fdf2ea2be63
Bayu S. Ajie P. Putra <bayu.setya.ajie@gmail.com>
2024-06-11 10:58:41 +0700

    try many conditional in construct function

--- app/Controllers/RiwayatLogin.php
+++ app/Controllers/RiwayatLogin.php
@@ -4,6 +4,7 @@ namespace App\Controllers;
 
 use App\Models\M_Laboran;
 use App\Models\M_Riwayat_Login;
+use App\Models\M_Users_Access_Menu;
 
 class RiwayatLogin extends BaseController
 {
@@ -14,30 +15,49 @@ class RiwayatLogin extends BaseController
 
   public function __construct()
   {
-    $this->riwayat = new M_Riwayat_Login();
-    if (session()->get('jenis_akses') != 'laboran') {
-      header("Location: " . base_url());
-    }
-    if (session()->get('nip_laboran')) {
-      $this->laboran = new M_Laboran();
-      $nip                = session()->get('nip_laboran');
-      $dataLaboran        = $this->laboran->getDataLaboran($nip);
-      $this->data = array(
-        'nip_laboran'   => $dataLaboran['nip_laboran'],
-        'nama_laboran'  => $dataLaboran['nama_laboran'],
-        'foto_laboran'  => $dataLaboran['foto_laboran']
-      );
-    } else {
+    // $model  = new M_Users_Access_Menu();
+    // $menu   = $model->getAccessMenuByLink(session()->get('id_role'), 'RiwayatLogin');
+    // if ($menu['is_active'] == '1') {
+    //   $this->riwayat = new M_Riwayat_Login();
+    //   if (session()->get('id_role') != '1') {
+    //     header("Location: " . base_url());
+    //     die();
+    //   }
+    //   if (session()->get('nip_laboran')) {
+    //     $this->laboran = new M_Laboran();
+    //     $nip                = session()->get('nip_laboran');
+    //     $dataLaboran        = $this->laboran->getDataLaboran($nip);
+    //     $this->data = array(
+    //       'nip_laboran'   => $dataLaboran['nip_laboran'],
+    //       'nama_laboran'  => $dataLaboran['nama_laboran'],
+    //       'foto_laboran'  => $dataLaboran['foto_laboran']
+    //     );
+    //   } else {
+    //     header("Location: " . base_url());
+    //     die();
+    //   }
+    // } else {
+    //   header("Location: " . base_url());
+    //   die();
+    // }
+    if (session()->get('login') != 'login') {
       header("Location: " . base_url());
       die();
+    } else {
+      if (session()->get('id_role') != '1') {
+        header("Location: " . base_url());
+        die();
+      } else {
+        echo 'laboran/admin';
+      }
     }
   }
 
   public function index()
   {
-    $data = $this->data;
-    $data['title']  = 'Riwayat Login | SIM Laboratorium';
-    $data['data_login'] = $this->riwayat->getHistoryLogin(session()->get('username'));
-    return view('riwayat_login/v_index', $data);
+    // $data = $this->data;
+    // $data['title']  = 'Riwayat Login | SIM Laboratorium';
+    // $data['data_login'] = $this->riwayat->getHistoryLogin(session()->get('username'));
+    // return view('riwayat_login/v_index', $data);
   }
 }
