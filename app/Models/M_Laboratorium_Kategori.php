6f0c7cd07e729454b85fe722396f7381699ee535
Bayu S. Ajie P. Putra <bayu.setya.ajie@gmail.com>
2024-06-11 17:04:37 +0700

    add model laboratorium kategori

---
+++ app/Models/M_Laboratorium_Kategori.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class M_Laboratorium_Kategori extends Model
+{
+
+  protected $table = 'laboratorium_kategori';
+  protected $allowedFields = ['id_lab_kategori', 'kategori_lab'];
+
+  public function getDataLaboraotirumKategori()
+  {
+    return $this->findAll();
+  }
+}
