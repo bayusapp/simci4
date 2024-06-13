e55a065a14773e91df6afefa22897a29b3e5ef70
Bayu S. Ajie P. Putra <bayu.setya.ajie@gmail.com>
2024-06-13 17:38:55 +0700

    add function to load data lab praktikum

--- app/Models/M_Laboratorium.php
+++ app/Models/M_Laboratorium.php
@@ -15,4 +15,14 @@ class M_Laboratorium extends Model
     $this->orderBy("kode_lab");
     return $this->findAll();
   }
+
+  public function getDataLabPraktikum()
+  {
+    $this->select('laboratorium.id_lab, laboratorium.nama_lab, laboratorium.kode_lab, laboratorium.kode_ruang, laboratorium_lokasi.lokasi, prodi.nama_prodi, prodi.jenjang_prodi');
+    $this->join('laboratorium_lokasi', 'laboratorium.id_lab_lokasi = laboratorium_lokasi.id_lab_lokasi');
+    $this->join('prodi', 'laboratorium.id_prodi = prodi.id_prodi');
+    $this->where('laboratorium.id_lab_kategori', '1');
+    $this->orderBy('laboratorium.kode_lab', 'asc');
+    return $this->findAll();
+  }
 }
