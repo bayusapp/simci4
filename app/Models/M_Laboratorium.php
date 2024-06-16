444c4ed631689b5093321d438668c86c1a10ed21
Bayu S. Ajie P. Putra <bayu.setya.ajie@gmail.com>
2024-06-16 23:05:26 +0700

    add function to update and delete data

--- app/Models/M_Laboratorium.php
+++ app/Models/M_Laboratorium.php
@@ -8,7 +8,8 @@ class M_Laboratorium extends Model
 {
 
   protected $table = 'laboratorium';
-  protected $allowedFields  = ['nama_lab', 'kode_lab', 'kode_igracias', 'kode_ruang', 'id_lab_kategori', 'id_lab_lokasi', 'id_prodi'];
+  protected $primaryKey = 'id_lab';
+  protected $allowedFields  = ['id_lab', 'nama_lab', 'kode_lab', 'kode_igracias', 'kode_ruang', 'id_lab_kategori', 'id_lab_lokasi', 'id_prodi'];
 
   public function getAllLaboratorium()
   {
@@ -18,11 +19,22 @@ class M_Laboratorium extends Model
 
   public function getDataLabPraktikum()
   {
-    $this->select('laboratorium.id_lab, laboratorium.nama_lab, laboratorium.kode_lab, laboratorium.kode_ruang, laboratorium_lokasi.lokasi, prodi.nama_prodi, prodi.jenjang_prodi');
+    $this->select('laboratorium.id_lab, laboratorium.nama_lab, laboratorium.kode_lab, laboratorium.kode_ruang, laboratorium.id_lab_kategori, laboratorium.id_lab_lokasi, laboratorium.id_prodi, laboratorium_lokasi.lokasi, prodi.nama_prodi, prodi.jenjang_prodi');
     $this->join('laboratorium_lokasi', 'laboratorium.id_lab_lokasi = laboratorium_lokasi.id_lab_lokasi');
     $this->join('prodi', 'laboratorium.id_prodi = prodi.id_prodi');
     $this->where('laboratorium.id_lab_kategori', '1');
     $this->orderBy('laboratorium.kode_lab', 'asc');
     return $this->findAll();
+  }
+
+  public function updateDataLab($id, $data)
+  {
+    return $this->update($id, $data);
+  }
+
+  public function deleteDataLab($id)
+  {
+    $this->where('substr(sha1(id_lab), 8, 7)', $id);
+    return $this->delete();
   }
 }
