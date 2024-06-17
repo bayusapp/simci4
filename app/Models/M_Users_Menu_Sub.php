d385a570e2c8f2761fb61304bbabf91dbe962e29
Bayu S. Ajie P. Putra <bayu.setya.ajie@gmail.com>
2024-06-17 12:24:25 +0700

    add function to get data sub menu by segment

--- app/Models/M_Users_Menu_Sub.php
+++ app/Models/M_Users_Menu_Sub.php
@@ -8,11 +8,18 @@ class M_Users_Menu_Sub extends Model
 {
 
   protected $table = 'users_menu_sub';
-  protected $allowedFields = ['nama_menu', 'url_menu', 'is_active', 'id_menu'];
+  protected $allowedFields = ['nama_menu', 'url_menu', 'urutan_menu', 'is_active', 'id_menu'];
 
   public function getDataSubMenu($id_menu)
   {
     $this->where('id_menu', $id_menu);
+    $this->orderBy('urutan_menu', 'asc');
     return $this->findAll();
+  }
+
+  public function getDataSubMenuSegment($segment_1, $segment_2)
+  {
+    $this->where('url_menu', $segment_1 . '/' . $segment_2);
+    return $this->first();
   }
 }
