6519f65143c47116995949c08e4ce933181ad62f
Bayu S. Ajie P. Putra <bayu.setya.ajie@gmail.com>
2024-06-17 12:23:54 +0700

    add js for plugin

---
+++ public/assets/js/simlab.js
@@ -0,0 +1,79 @@
+$(document).ready(function() {
+  setTimeout(function() {
+    $('#mk_si').DataTable({
+      bAutoWidth: false,
+      columnDefs: [
+        {"width": "5%", "targets": [0]},
+        {"width": "15%", "targets": [1]},
+        {"width": "12%", "targets": [3]},
+      ]
+    });
+
+    $('#mk_tk').DataTable({
+      bAutoWidth: false,
+      columnDefs: [
+        {"width": "5%", "targets": [0]},
+        {"width": "15%", "targets": [1]},
+        {"width": "12%", "targets": [3]},
+      ]
+    });
+
+    $('#mk_sia').DataTable({
+      bAutoWidth: false,
+      columnDefs: [
+        {"width": "5%", "targets": [0]},
+        {"width": "15%", "targets": [1]},
+        {"width": "12%", "targets": [3]},
+      ]
+    });
+
+    $('#mk_mp').DataTable({
+      bAutoWidth: false,
+      columnDefs: [
+        {"width": "5%", "targets": [0]},
+        {"width": "15%", "targets": [1]},
+        {"width": "12%", "targets": [3]},
+      ]
+    });
+
+    $('#mk_tt').DataTable({
+      bAutoWidth: false,
+      columnDefs: [
+        {"width": "5%", "targets": [0]},
+        {"width": "15%", "targets": [1]},
+        {"width": "12%", "targets": [3]},
+      ]
+    });
+
+    $('#mk_rpla').DataTable({
+      bAutoWidth: false,
+      columnDefs: [
+        {"width": "5%", "targets": [0]},
+        {"width": "15%", "targets": [1]},
+        {"width": "12%", "targets": [3]},
+      ]
+    });
+
+    $('#mk_ph').DataTable({
+      bAutoWidth: false,
+      columnDefs: [
+        {"width": "5%", "targets": [0]},
+        {"width": "15%", "targets": [1]},
+        {"width": "12%", "targets": [3]},
+      ]
+    });
+
+    $('#mk_trm').DataTable({
+      bAutoWidth: false,
+      columnDefs: [
+        {"width": "5%", "targets": [0]},
+        {"width": "15%", "targets": [1]},
+        {"width": "12%", "targets": [3]},
+      ]
+    });
+  }, 0);
+
+  $(".id_prodi").select2({
+    placeholder: "Pilih Program Studi"
+  });
+});
