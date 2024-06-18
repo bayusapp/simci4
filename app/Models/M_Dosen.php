c48342c948343c409799b3babafa127ad3e0dc76
Bayu S. Ajie P. Putra <bayu.setya.ajie@gmail.com>
2024-06-18 22:48:51 +0700

    create model for dosen

---
+++ app/Models/M_Dosen.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class M_Dosen extends Model
+{
+
+  protected $table = 'dosen';
+  protected $primaryKey = 'kode_dosen';
+  protected $allowedFields  = ['kode_dosen', 'nama_dosen'];
+}
