5e12916bd0cc8d1175b24361777ef5dcee6432bd
Bayu S. Ajie P. Putra <bayu.setya.ajie@gmail.com>
2024-06-18 17:38:32 +0700

    add function update & delete program studi

--- app/Models/M_Prodi.php
+++ app/Models/M_Prodi.php
@@ -8,10 +8,24 @@ class M_Prodi extends Model
 {
 
   protected $table = 'prodi';
+  protected $primaryKey = 'id_prodi';
   protected $allowedFields = ['nama_prodi', 'jenjang_prodi', 'kode_prodi'];
 
   public function getDataProdi()
   {
     return $this->findAll();
+  }
+
+  public function updateProdi($id_prodi, $nama_prodi, $jenjang_prodi, $kode_prodi)
+  {
+    $db = db_connect();
+    $query  = "UPDATE prodi SET nama_prodi = '{$nama_prodi}', jenjang_prodi = '{$jenjang_prodi}', kode_prodi = '{$kode_prodi}' WHERE SUBSTR(SHA1(id_prodi), 8, 7) = '{$id_prodi}'";
+    return $db->query($query);
+  }
+
+  public function deleteProdi($id)
+  {
+    $this->where('substr(sha1(id_prodi), 8, 7)', $id);
+    return $this->delete();
   }
 }
