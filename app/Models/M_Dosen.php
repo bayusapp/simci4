4e626852028c49e169402c1f0bf994d4040542d8
Bayu S. Ajie P. Putra <bayu.setya.ajie@gmail.com>
2024-06-20 18:44:43 +0700

    add function to delete data dosen

--- app/Models/M_Dosen.php
+++ app/Models/M_Dosen.php
@@ -22,4 +22,10 @@ class M_Dosen extends Model
     $query = "UPDATE dosen SET kode_dosen = '{$kode_dosen}', nama_dosen = '{$nama_dosen}' WHERE SUBSTR(SHA1(kode_dosen), 8, 7) = '{$kode_old}'";
     return $db->query($query);
   }
+
+  public function deleteDosen($kode_dosen)
+  {
+    $this->where('SUBSTR(SHA1(kode_dosen), 8, 7)', $kode_dosen);
+    return $this->delete();
+  }
 }
