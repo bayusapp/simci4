c4d18f4a7678e597bc155d6fbaaf95ba6ad50abd
Bayu S. Ajie P. Putra <bayu.setya.ajie@gmail.com>
2024-06-28 00:30:35 +0700

    fix bug for gelar dosen

--- app/Controllers/DataMaster.php
+++ app/Controllers/DataMaster.php
@@ -131,7 +131,7 @@ class DataMaster extends BaseController
       $split_gelar  = explode(",", $nama_dosen);
       $gelar = '';
       for ($i = 1; $i < count($split_gelar); $i++) {
-        $gelar .= ', ' . $split_gelar[$i];
+        $gelar .= ',' . $split_gelar[$i];
       }
       $split_nama = explode(" ", $split_gelar[0]);
       for ($i = 0; $i < count($split_nama); $i++) {
@@ -178,7 +178,7 @@ class DataMaster extends BaseController
       $hash_kode_dosen  = substr(sha1($kode_dosen), 7, 7);
       $gelar = '';
       for ($i = 1; $i < count($split_gelar); $i++) {
-        $gelar .= ', ' . $split_gelar[$i];
+        $gelar .= ',' . $split_gelar[$i];
       }
       $split_nama = explode(" ", $split_gelar[0]);
       for ($i = 0; $i < count($split_nama); $i++) {
@@ -244,7 +244,7 @@ class DataMaster extends BaseController
         $split_gelar  = explode(",", $nama_dosen);
         $gelar = '';
         for ($i = 1; $i < count($split_gelar); $i++) {
-          $gelar .= ', ' . $split_gelar[$i];
+          $gelar .= ',' . $split_gelar[$i];
         }
         $split_nama = explode(" ", $split_gelar[0]);
         for ($i = 0; $i < count($split_nama); $i++) {
