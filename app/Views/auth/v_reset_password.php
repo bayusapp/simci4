974b183693ad71ce7c482197cc8f0e1f767fbab7
Bayu S. Ajie P. Putra <bayu.setya.ajie@gmail.com>
2024-07-07 14:20:12 +0700

    add view for lupa password

---
+++ app/Views/auth/v_reset_password.php
@@ -0,0 +1,32 @@
+<?= $this->extend('auth/v_template') ?>
+<?= $this->section('content') ?>
+<div class="auth-side-form">
+  <div class=" auth-content">
+    <img src="<?= base_url() ?>assets/images/auth/auth-logo-dark.png" alt="" class="img-fluid mb-4 d-block d-xl-none d-lg-none">
+    <?php if (!empty(session()->getFlashdata('sukses'))) : ?>
+      <div class="alert alert-success" role="alert">
+        <?= session()->getFlashdata('sukses') ?>
+      </div>
+    <?php elseif (!empty(session()->getFlashdata('error'))) : ?>
+      <div class="alert alert-danger" role="alert">
+        <?= session()->getFlashdata('error') ?>
+      </div>
+    <?php endif; ?>
+    <h4 class="mb-3 f-w-400">Reset Password</h4>
+    <form method="post" action="<?= base_url('Auth/submitReset') ?>">
+      <div class="form-group mb-4">
+        <label for="id">Username/NIP/NIM</label>
+        <input type="text" class="form-control" name="id" id="id" value="<?= old('id') ?>" placeholder="Contoh: johndoe">
+      </div>
+      <input type="text" name="location" id="location" class="form-control" readonly hidden>
+      <div id='map' hidden></div>
+      <button type="submit" class="btn btn-block btn-primary mb-4">Reset Password</button>
+    </form>
+    <div class="text-center">
+      <div class="saprator my-4"><span>ATAU</span></div>
+      <p class="mb-2 mt-4 text-muted">Sudah punya akun? <a href="auth-reset-password-img-side.html" class="f-w-400">Login</a></p>
+      <p class="mb-0 text-muted">Belum punya akun? <a href="auth-signup-img-side.html" class="f-w-400">Register Akun</a></p>
+    </div>
+  </div>
+</div>
+<?= $this->endSection('content') ?>
