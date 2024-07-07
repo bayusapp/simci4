b628d455ca1f1a9788f909c6f0bc44526eb541bf
Bayu S. Ajie P. Putra <bayu.setya.ajie@gmail.com>
2024-07-07 14:20:40 +0700

    create model for forgot password

---
+++ app/Models/M_Forgot_Password.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class M_Forgot_Password extends Model
+{
+
+  protected $table = 'forgot_password';
+  protected $primaryKey = 'id_forgot_password';
+  protected $allowedFields = ['username', 'token', 'tanggal', 'status'];
+
+  public function checkToken($token)
+  {
+    $this->where('token', $token);
+    return $this->first();
+  }
+}
