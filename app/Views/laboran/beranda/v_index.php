7c24e28c72816410a9822e4df164109ab487bb7f
Bayu S. Ajie P. Putra <bayu.setya.ajie@gmail.com>
2024-07-09 16:02:57 +0700

    move breadcrumb from view to template

--- app/Views/laboran/beranda/v_index.php
+++ app/Views/laboran/beranda/v_index.php
@@ -1,212 +1,204 @@
 <?= $this->extend('template/v_template') ?>
 <?= $this->section('content') ?>
 <!-- [ Main Content ] start -->
-<div class="pcoded-main-container">
-  <div class="pcoded-content">
-    <!-- [ breadcrumb ] start -->
-    <div class="page-header">
-    </div>
-    <!-- [ breadcrumb ] end -->
-    <!-- [ Main Content ] start -->
+<!-- [ Main Content ] start -->
+<div class="row">
+  <div class="col-lg-3 col-md-12">
+    <!-- support-section start -->
     <div class="row">
-      <div class="col-lg-3 col-md-12">
-        <!-- support-section start -->
-        <div class="row">
-          <div class="col-sm-12">
-            <div class="card support-bar overflow-hidden">
-              <div class="card-body pb-0">
-                <h2 class="m-0">350</h2>
-                <p class="mb-3 mt-3">Total Trouble Ticket dalam <?= date('Y') ?></p>
-              </div>
-              <div id="support-chart"></div>
-              <div class="card-footer bg-primary text-white">
-                <div class="row text-center">
-                  <div class="col">
-                    <h4 class="m-0 text-white">999</h4>
-                    <span>Buka</span>
-                  </div>
-                  <div class="col">
-                    <h4 class="m-0 text-white">5</h4>
-                    <span>Berjalan</span>
-                  </div>
-                  <div class="col">
-                    <h4 class="m-0 text-white">3</h4>
-                    <span>Selesai</span>
-                  </div>
-                </div>
-              </div>
-            </div>
+      <div class="col-sm-12">
+        <div class="card support-bar overflow-hidden">
+          <div class="card-body pb-0">
+            <h2 class="m-0">350</h2>
+            <p class="mb-3 mt-3">Total Trouble Ticket dalam <?= date('Y') ?></p>
           </div>
-          <div class="col-sm-12">
-            <div class="card support-bar overflow-hidden">
-              <div class="card-body pb-0">
-                <h2 class="m-0">350</h2>
-                <p class="mb-3 mt-3">Total Trouble Ticket dalam <?= date('Y') ?></p>
+          <div id="support-chart"></div>
+          <div class="card-footer bg-primary text-white">
+            <div class="row text-center">
+              <div class="col">
+                <h4 class="m-0 text-white">999</h4>
+                <span>Buka</span>
               </div>
-              <div id="support-chart"></div>
-              <div class="card-footer bg-primary text-white">
-                <div class="row text-center">
-                  <div class="col">
-                    <h4 class="m-0 text-white">999</h4>
-                    <span>Buka</span>
-                  </div>
-                  <div class="col">
-                    <h4 class="m-0 text-white">5</h4>
-                    <span>Berjalan</span>
-                  </div>
-                  <div class="col">
-                    <h4 class="m-0 text-white">3</h4>
-                    <span>Selesai</span>
-                  </div>
-                </div>
+              <div class="col">
+                <h4 class="m-0 text-white">5</h4>
+                <span>Berjalan</span>
+              </div>
+              <div class="col">
+                <h4 class="m-0 text-white">3</h4>
+                <span>Selesai</span>
               </div>
             </div>
           </div>
         </div>
-        <!-- support-section end -->
       </div>
-      <div class="col-lg-9 col-md-12">
-        <!-- page statustic card start -->
-        <div class="row">
-          <div class="col-sm-12">
-            <div class="card table-card latest-activity-card">
-              <div class="card-header">
-                <h5>Biaya Honor Asisten Praktikum <?= date('Y') ?></h5>
+      <div class="col-sm-12">
+        <div class="card support-bar overflow-hidden">
+          <div class="card-body pb-0">
+            <h2 class="m-0">350</h2>
+            <p class="mb-3 mt-3">Total Trouble Ticket dalam <?= date('Y') ?></p>
+          </div>
+          <div id="support-chart"></div>
+          <div class="card-footer bg-primary text-white">
+            <div class="row text-center">
+              <div class="col">
+                <h4 class="m-0 text-white">999</h4>
+                <span>Buka</span>
               </div>
-              <div class="card-body p-0">
-                <div class="table-responsive honor_asprak">
-                  <table id="honor_asprak" class="table table-striped table-borderless mb-0" style="width: 100%;" cellspacing="0">
-                    <thead>
-                      <tr>
-                        <th>No</th>
-                        <th>Program Studi</th>
-                        <th colspan="2">Triwulan 1</th>
-                        <th colspan="2">Triwulan 2</th>
-                        <th colspan="2">Triwulan 3</th>
-                        <th colspan="2">Triwulan 4</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <td>1</td>
-                        <td>D3 Sistem Informasi</td>
-                        <td style="text-align: left;">Rp </td>
-                        <td style="text-align: right;">86.000.000</td>
-                        <td style="text-align: left;">Rp </td>
-                        <td style="text-align: right;">86.000.000</td>
-                        <td style="text-align: left;">Rp </td>
-                        <td style="text-align: right;">86.000.000</td>
-                        <td style="text-align: left;">Rp </td>
-                        <td style="text-align: right;">86.000.000</td>
-                      </tr>
-                      <tr>
-                        <td>2</td>
-                        <td>D3 Teknologi Komputer</td>
-                        <td style="text-align: left;">Rp </td>
-                        <td style="text-align: right;">86.000.000</td>
-                        <td style="text-align: left;">Rp </td>
-                        <td style="text-align: right;">86.000.000</td>
-                        <td style="text-align: left;">Rp </td>
-                        <td style="text-align: right;">86.000.000</td>
-                        <td style="text-align: left;">Rp </td>
-                        <td style="text-align: right;">86.000.000</td>
-                      </tr>
-                      <tr>
-                        <td>3</td>
-                        <td>D3 Sistem Informasi Akuntansi</td>
-                        <td style="text-align: left;">Rp </td>
-                        <td style="text-align: right;">86.000.000</td>
-                        <td style="text-align: left;">Rp </td>
-                        <td style="text-align: right;">86.000.000</td>
-                        <td style="text-align: left;">Rp </td>
-                        <td style="text-align: right;">86.000.000</td>
-                        <td style="text-align: left;">Rp </td>
-                        <td style="text-align: right;">86.000.000</td>
-                      </tr>
-                      <tr>
-                        <td>4</td>
-                        <td>D3 Manajemen Pemasaran</td>
-                        <td style="text-align: left;">Rp </td>
-                        <td style="text-align: right;">86.000.000</td>
-                        <td style="text-align: left;">Rp </td>
-                        <td style="text-align: right;">86.000.000</td>
-                        <td style="text-align: left;">Rp </td>
-                        <td style="text-align: right;">86.000.000</td>
-                        <td style="text-align: left;">Rp </td>
-                        <td style="text-align: right;">86.000.000</td>
-                      </tr>
-                      <tr>
-                        <td>5</td>
-                        <td>D3 Teknologi Telekomunikasi</td>
-                        <td style="text-align: left;">Rp </td>
-                        <td style="text-align: right;">86.000.000</td>
-                        <td style="text-align: left;">Rp </td>
-                        <td style="text-align: right;">86.000.000</td>
-                        <td style="text-align: left;">Rp </td>
-                        <td style="text-align: right;">86.000.000</td>
-                        <td style="text-align: left;">Rp </td>
-                        <td style="text-align: right;">86.000.000</td>
-                      </tr>
-                      <tr>
-                        <td>5</td>
-                        <td>D3 Teknologi Telekomunikasi</td>
-                        <td style="text-align: left;">Rp </td>
-                        <td style="text-align: right;">86.000.000</td>
-                        <td style="text-align: left;">Rp </td>
-                        <td style="text-align: right;">86.000.000</td>
-                        <td style="text-align: left;">Rp </td>
-                        <td style="text-align: right;">86.000.000</td>
-                        <td style="text-align: left;">Rp </td>
-                        <td style="text-align: right;">86.000.000</td>
-                      </tr>
-                      <tr>
-                        <td>5</td>
-                        <td>D3 Teknologi Telekomunikasi</td>
-                        <td style="text-align: left;">Rp </td>
-                        <td style="text-align: right;">86.000.000</td>
-                        <td style="text-align: left;">Rp </td>
-                        <td style="text-align: right;">86.000.000</td>
-                        <td style="text-align: left;">Rp </td>
-                        <td style="text-align: right;">86.000.000</td>
-                        <td style="text-align: left;">Rp </td>
-                        <td style="text-align: right;">86.000.000</td>
-                      </tr>
-                      <tr>
-                        <td>5</td>
-                        <td>D3 Teknologi Telekomunikasi</td>
-                        <td style="text-align: left;">Rp </td>
-                        <td style="text-align: right;">86.000.000</td>
-                        <td style="text-align: left;">Rp </td>
-                        <td style="text-align: right;">86.000.000</td>
-                        <td style="text-align: left;">Rp </td>
-                        <td style="text-align: right;">86.000.000</td>
-                        <td style="text-align: left;">Rp </td>
-                        <td style="text-align: right;">86.000.000</td>
-                      </tr>
-                      <tr>
-                        <td>5</td>
-                        <td>D3 Teknologi Telekomunikasi</td>
-                        <td style="text-align: left;">Rp </td>
-                        <td style="text-align: right;">86.000.000</td>
-                        <td style="text-align: left;">Rp </td>
-                        <td style="text-align: right;">86.000.000</td>
-                        <td style="text-align: left;">Rp </td>
-                        <td style="text-align: right;">86.000.000</td>
-                        <td style="text-align: left;">Rp </td>
-                        <td style="text-align: right;">86.000.000</td>
-                      </tr>
-                    </tbody>
-                  </table>
-                </div>
+              <div class="col">
+                <h4 class="m-0 text-white">5</h4>
+                <span>Berjalan</span>
+              </div>
+              <div class="col">
+                <h4 class="m-0 text-white">3</h4>
+                <span>Selesai</span>
               </div>
             </div>
           </div>
         </div>
-        <!-- page statustic card end -->
       </div>
     </div>
-    <!-- [ Main Content ] end -->
+    <!-- support-section end -->
+  </div>
+  <div class="col-lg-9 col-md-12">
+    <!-- page statustic card start -->
+    <div class="row">
+      <div class="col-sm-12">
+        <div class="card table-card latest-activity-card">
+          <div class="card-header">
+            <h5>Biaya Honor Asisten Praktikum <?= date('Y') ?></h5>
+          </div>
+          <div class="card-body p-0">
+            <div class="table-responsive honor_asprak">
+              <table id="honor_asprak" class="table table-striped table-borderless mb-0" style="width: 100%;" cellspacing="0">
+                <thead>
+                  <tr>
+                    <th>No</th>
+                    <th>Program Studi</th>
+                    <th colspan="2">Triwulan 1</th>
+                    <th colspan="2">Triwulan 2</th>
+                    <th colspan="2">Triwulan 3</th>
+                    <th colspan="2">Triwulan 4</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>1</td>
+                    <td>D3 Sistem Informasi</td>
+                    <td style="text-align: left;">Rp </td>
+                    <td style="text-align: right;">86.000.000</td>
+                    <td style="text-align: left;">Rp </td>
+                    <td style="text-align: right;">86.000.000</td>
+                    <td style="text-align: left;">Rp </td>
+                    <td style="text-align: right;">86.000.000</td>
+                    <td style="text-align: left;">Rp </td>
+                    <td style="text-align: right;">86.000.000</td>
+                  </tr>
+                  <tr>
+                    <td>2</td>
+                    <td>D3 Teknologi Komputer</td>
+                    <td style="text-align: left;">Rp </td>
+                    <td style="text-align: right;">86.000.000</td>
+                    <td style="text-align: left;">Rp </td>
+                    <td style="text-align: right;">86.000.000</td>
+                    <td style="text-align: left;">Rp </td>
+                    <td style="text-align: right;">86.000.000</td>
+                    <td style="text-align: left;">Rp </td>
+                    <td style="text-align: right;">86.000.000</td>
+                  </tr>
+                  <tr>
+                    <td>3</td>
+                    <td>D3 Sistem Informasi Akuntansi</td>
+                    <td style="text-align: left;">Rp </td>
+                    <td style="text-align: right;">86.000.000</td>
+                    <td style="text-align: left;">Rp </td>
+                    <td style="text-align: right;">86.000.000</td>
+                    <td style="text-align: left;">Rp </td>
+                    <td style="text-align: right;">86.000.000</td>
+                    <td style="text-align: left;">Rp </td>
+                    <td style="text-align: right;">86.000.000</td>
+                  </tr>
+                  <tr>
+                    <td>4</td>
+                    <td>D3 Manajemen Pemasaran</td>
+                    <td style="text-align: left;">Rp </td>
+                    <td style="text-align: right;">86.000.000</td>
+                    <td style="text-align: left;">Rp </td>
+                    <td style="text-align: right;">86.000.000</td>
+                    <td style="text-align: left;">Rp </td>
+                    <td style="text-align: right;">86.000.000</td>
+                    <td style="text-align: left;">Rp </td>
+                    <td style="text-align: right;">86.000.000</td>
+                  </tr>
+                  <tr>
+                    <td>5</td>
+                    <td>D3 Teknologi Telekomunikasi</td>
+                    <td style="text-align: left;">Rp </td>
+                    <td style="text-align: right;">86.000.000</td>
+                    <td style="text-align: left;">Rp </td>
+                    <td style="text-align: right;">86.000.000</td>
+                    <td style="text-align: left;">Rp </td>
+                    <td style="text-align: right;">86.000.000</td>
+                    <td style="text-align: left;">Rp </td>
+                    <td style="text-align: right;">86.000.000</td>
+                  </tr>
+                  <tr>
+                    <td>5</td>
+                    <td>D3 Teknologi Telekomunikasi</td>
+                    <td style="text-align: left;">Rp </td>
+                    <td style="text-align: right;">86.000.000</td>
+                    <td style="text-align: left;">Rp </td>
+                    <td style="text-align: right;">86.000.000</td>
+                    <td style="text-align: left;">Rp </td>
+                    <td style="text-align: right;">86.000.000</td>
+                    <td style="text-align: left;">Rp </td>
+                    <td style="text-align: right;">86.000.000</td>
+                  </tr>
+                  <tr>
+                    <td>5</td>
+                    <td>D3 Teknologi Telekomunikasi</td>
+                    <td style="text-align: left;">Rp </td>
+                    <td style="text-align: right;">86.000.000</td>
+                    <td style="text-align: left;">Rp </td>
+                    <td style="text-align: right;">86.000.000</td>
+                    <td style="text-align: left;">Rp </td>
+                    <td style="text-align: right;">86.000.000</td>
+                    <td style="text-align: left;">Rp </td>
+                    <td style="text-align: right;">86.000.000</td>
+                  </tr>
+                  <tr>
+                    <td>5</td>
+                    <td>D3 Teknologi Telekomunikasi</td>
+                    <td style="text-align: left;">Rp </td>
+                    <td style="text-align: right;">86.000.000</td>
+                    <td style="text-align: left;">Rp </td>
+                    <td style="text-align: right;">86.000.000</td>
+                    <td style="text-align: left;">Rp </td>
+                    <td style="text-align: right;">86.000.000</td>
+                    <td style="text-align: left;">Rp </td>
+                    <td style="text-align: right;">86.000.000</td>
+                  </tr>
+                  <tr>
+                    <td>5</td>
+                    <td>D3 Teknologi Telekomunikasi</td>
+                    <td style="text-align: left;">Rp </td>
+                    <td style="text-align: right;">86.000.000</td>
+                    <td style="text-align: left;">Rp </td>
+                    <td style="text-align: right;">86.000.000</td>
+                    <td style="text-align: left;">Rp </td>
+                    <td style="text-align: right;">86.000.000</td>
+                    <td style="text-align: left;">Rp </td>
+                    <td style="text-align: right;">86.000.000</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- page statustic card end -->
   </div>
 </div>
+<!-- [ Main Content ] end -->
 <!-- Button trigger modal -->
 <?= $this->endSection('content') ?>
