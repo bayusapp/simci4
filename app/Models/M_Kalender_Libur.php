40a10ab447b85c8fd4eeee0196de7f20c4cf4d5e
Bayu S. Ajie P. Putra <bayu.setya.ajie@gmail.com>
2024-07-09 10:26:37 +0700

    add some function to crud kalender

--- app/Models/M_Kalender_Libur.php
+++ app/Models/M_Kalender_Libur.php
@@ -16,4 +16,29 @@ class M_Kalender_Libur extends Model
     $this->where("tanggal like '{$tahun}%'");
     return $this->findAll();
   }
+
+  public function checkDataKalender($tanggal)
+  {
+    $this->where('tanggal', $tanggal);
+    return $this->first();
+  }
+
+  public function checkDataKalenderByID($id)
+  {
+    $this->where("substr(sha1(id_tanggal), 8, 7) = '{$id}'");
+    return $this->first();
+  }
+
+  public function updateKalender($id_tanggal, $tanggal, $keterangan)
+  {
+    $db = db_connect();
+    $query = "UPDATE kalender_libur SET tanggal = '{$tanggal}', keterangan = '{$keterangan}' WHERE substr(sha1(id_tanggal), 8, 7) = '{$id_tanggal}'";
+    return $db->query($query);
+  }
+
+  public function deleteKalender($id)
+  {
+    $this->where("substr(sha1(id_tanggal), 8, 7) = '{$id}'");
+    return $this->delete();
+  }
 }
