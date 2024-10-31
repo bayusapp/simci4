7a5457e895f20a5bccf00eebffbd0aea52387f4a
Bayu S. Ajie P. Putra <bayu.setya.ajie@gmail.com>
2024-10-31 18:21:28 +0700

    change view 404 error page

--- app/Views/errors/html/error_404.php
+++ app/Views/errors/html/error_404.php
@@ -2,94 +2,44 @@
 <html lang="en">
 
 <head>
+  <title>Ablepro v8.0 bootstrap admin template by Phoenixcoded</title>
   <meta charset="utf-8">
-  <title><?= lang('Errors.pageNotFound') ?></title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="description" content="" />
+  <meta name="keywords" content="">
+  <meta name="author" content="Phoenixcoded" />
+  <!-- Favicon icon -->
+  <link rel="icon" href="<?= base_url() ?>assets/images/favicon.ico" type="image/x-icon">
 
-  <style>
-    div.logo {
-      height: 200px;
-      width: 155px;
-      display: inline-block;
-      opacity: 0.08;
-      position: absolute;
-      top: 2rem;
-      left: 50%;
-      margin-left: -73px;
-    }
+  <!-- vendor css -->
+  <link rel="stylesheet" href="<?= base_url() ?>assets/css/style.css">
 
-    body {
-      height: 100%;
-      background: #fafafa;
-      font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
-      color: #777;
-      font-weight: 300;
-    }
 
-    h1 {
-      font-weight: lighter;
-      letter-spacing: normal;
-      font-size: 3rem;
-      margin-top: 0;
-      margin-bottom: 0;
-      color: #222;
-    }
 
-    .wrap {
-      max-width: 1024px;
-      margin: 5rem auto;
-      padding: 2rem;
-      background: #fff;
-      text-align: center;
-      border: 1px solid #efefef;
-      border-radius: 0.5rem;
-      position: relative;
-    }
-
-    pre {
-      white-space: normal;
-      margin-top: 1.5rem;
-    }
-
-    code {
-      background: #fafafa;
-      border: 1px solid #efefef;
-      padding: 0.5rem 1rem;
-      border-radius: 5px;
-      display: block;
-    }
-
-    p {
-      margin-top: 1.5rem;
-    }
-
-    .footer {
-      margin-top: 2rem;
-      border-top: 1px solid #efefef;
-      padding: 1em 2em 0 2em;
-      font-size: 85%;
-      color: #999;
-    }
-
-    a:active,
-    a:link,
-    a:visited {
-      color: #dd4814;
-    }
-  </style>
 </head>
-
-<body>
-  <div class="wrap">
-    <h1>404</h1>
-
-    <p>
-      <?php if (ENVIRONMENT !== 'production') : ?>
-        <?= nl2br(esc($message)) ?>
-      <?php else : ?>
-        <?= lang('Errors.sorryCannotFind') ?>
-      <?php endif; ?>
-    </p>
+<!-- [ offline-ui ] start -->
+<div class="auth-wrapper maintance">
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-8">
+        <div class="text-center">
+          <img src="<?= base_url() ?>assets/images/maintance/404.png" alt="" class="img-fluid">
+          <h5 class="text-muted my-4">Halaman tidak ditemukan</h5>
+          <a href="#" onclick="window.history.back(); return false;">
+            <button class="btn waves-effect waves-light btn-primary mb-4"><i class="feather icon-arrow-left mr-2"></i>Kembali</button>
+          </a>
+        </div>
+      </div>
+    </div>
   </div>
+</div>
+<!-- [ offline-ui ] end -->
+<!-- Required Js -->
+<script src="assets/js/vendor-all.min.js"></script>
+<script src="assets/js/plugins/bootstrap.min.js"></script>
+
+
 </body>
 
 </html>
